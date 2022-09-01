3512e49ea216085ed7a2fbf2f794cd1a5dd0b119
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2022-09-01 14:04:11 +0200

    added cors.

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
