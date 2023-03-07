805da0a8fe518d93e756daa5bf5804b9857408ee
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2023-03-07 13:46:26 +0100

    add password hashing 😼.

--- api/models/user.go
+++ api/models/user.go
@@ -6,19 +6,34 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	Id   primitive.ObjectID
-	Name string
+	Id       primitive.ObjectID
+	Name     string
+	Password string
+}
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 13)
+	return string(bytes), err
 }
 
 func CreateUser(newUser *User) error {
+	passwordHash, err := hashPassword(newUser.Password)
+
+	if err != nil {
+		return err
+	}
+
+	newUser.Password = passwordHash
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
 	defer cancel()
 
-	_, err := db.UserCollection.InsertOne(ctx, newUser)
+	_, err = db.UserCollection.InsertOne(ctx, newUser)
 
 	return err
 }
