14ec052d225024735810ef293db69b8b8be2cae2
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2023-03-07 12:50:44 +0100

    add create jwt func. 🤪

--- api/routes/api/auth.go
+++ api/routes/api/auth.go
@@ -1,12 +1,38 @@
 package api
 
 import (
+	"log"
 	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
+const secret = "1234"
+
+func createJWT() string {
+	claims := jwt.MapClaims{
+		"test": "123",
+		"iat":  time.Now().Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secret))
+
+	if err != nil {
+		log.Fatal(err)
+
+		return ""
+	}
+
+	return tokenString
+}
+
 func LoginHandle(c *gin.Context) {
+	token := createJWT()
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
+		"token": token,
 	})
 }
