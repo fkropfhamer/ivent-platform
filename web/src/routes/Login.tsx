a8f33139306b600697880ca592333b700dc0b001
Simon Engstler <SimonEngstler@live.de>
2023-03-12 15:36:16 +0100

    refactored login and register to tailwind

--- web/src/routes/Login.tsx
+++ web/src/routes/Login.tsx
@@ -1,9 +1,8 @@
 import {useState} from "react";
 import {useDispatch} from "react-redux";
-import {useNavigate} from "react-router-dom"
+import {useNavigate} from "react-router-dom";
 import {setRefreshToken, setToken} from "../features/auth/auth-slice";
 import {useLoginMutation} from "../services/api";
-import styles from '../styles/login.module.css'
 
 export const Login = () => {
     const navigate = useNavigate();
@@ -11,45 +10,57 @@ export const Login = () => {
     const dispatch = useDispatch();
 
     const [formState, setFormState] = useState({
-        username: '',
-        password: '',
+        username: "",
+        password: "",
     });
 
-    const onFormSubmit = async () => {
+    const onFormSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
         const tokens = await login(formState).unwrap();
-        dispatch(setToken(tokens.token))
-        dispatch(setRefreshToken(tokens["refresh-token"]))
+        dispatch(setToken(tokens.token));
+        dispatch(setRefreshToken(tokens["refresh-token"]));
 
         console.log(tokens);
 
-        navigate("/profile")
-    }
-
+        navigate("/profile");
+    };
 
     return (
-        <body>
-        <div className={styles["login-box"]}>
-            <h1 className={styles.h1}>Login</h1>
+        <div className="w-96 bg-gray-100 rounded-lg mx-auto my-20 p-8 border-2 border-gray-200">
+            <h1 className="text-3xl text-center font-bold mb-6">Login</h1>
             <form>
-                <label>Username</label>
+                <label className="block text-lg mb-4">Username</label>
                 <input
-                    type='text'
+                    type="text"
+                    className="w-full px-4 py-2 rounded-lg text-lg mb-4 border-2 border-gray-300"
                     value={formState.username}
-                    onChange={(e) => setFormState((prev) => ({...prev, username: e.target.value}))}
+                    onChange={(e) =>
+                        setFormState((prev) => ({...prev, username: e.target.value}))
+                    }
                 />
-                <label>Password</label>
+                <label className="block text-lg mb-4">Password</label>
                 <input
-                    type='text'
+                    type="password"
+                    className="w-full px-4 py-2 rounded-lg text-lg mb-6 border-2 border-gray-300"
                     value={formState.password}
-                    onChange={(e) => setFormState((prev) => ({...prev, password: e.target.value}))}
+                    onChange={(e) =>
+                        setFormState((prev) => ({...prev, password: e.target.value}))
+                    }
                 />
+                <button
+                    className="w-full bg-green-500 hover:bg-green-600 text-white rounded-lg py-2 text-lg mb-6"
+                    onClick={onFormSubmit}
+                >
+                    Submit
+                </button>
             </form>
-            <button
-                onClick={onFormSubmit}>Submit</button>
-            <p>
-                Don't have an account? <a href="/register">Register</a>
+            <p className="text-lg text-center">
+                Don't have an account?{' '}
+                <a href="/register" className="text-green-500 hover:underline">
+                    Register
+                </a>
             </p>
         </div>
-        </body>
-    )
-}
+    );
+};
