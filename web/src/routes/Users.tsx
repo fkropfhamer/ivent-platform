77319d04d6244d54338715a84df8993c880c293e
Simon Engstler <102315699+SirSiegmeyer@users.noreply.github.com>
2023-03-19 13:43:42 +0100

    added admin area and improved users visuals (#47)
    * added Admin Tools
    
    * renamed header
    
    * improved margin
    
    * improved users visuals

--- web/src/routes/Users.tsx
+++ web/src/routes/Users.tsx
@@ -23,12 +23,18 @@ export const Users = () => {
         return <div>Loading</div>
     }
 
-    return <>
-        <h1>Users</h1>
-        {users.map(user => <div key={user.id}>
-            <div>id: {user.id}</div>
-            <div>username: {user.name}</div>
-            <div>role: {user.role}</div>
-        </div>)}
-    </>
+    return (
+        <div className="w-full bg-gray-100 rounded-lg mx-auto my-20 p-8 border-2 border-gray-200">
+            <h1 className="text-3xl text-center font-bold mb-6">Users</h1>
+            {users.map(user => (
+                <div key={user.id} className="py-1 my-5 bg-gray-200 rounded-lg border border-gray-300">
+                    <div className="px-4 py-2 flex justify-between items-center">
+                        <div className="font-bold text-lg">{user.name}</div>
+                        <div className="text-sm font-medium text-gray-500">{user.role}</div>
+                    </div>
+                    <div className="px-4 py-2 text-gray-600 text-sm">{`ID: ${user.id}`}</div>
+                </div>
+            ))}
+        </div>
+    )
 }
