28248102f7331711e2f1e8fc3fcf9844b4a11246
Simon Engstler <SimonEngstler@live.de>
2023-03-21 11:26:10 +0100

    adjusted endpoints and autoformat

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,5 +1,5 @@
-import { BaseQueryFn, createApi, FetchArgs, fetchBaseQuery, FetchBaseQueryError } from '@reduxjs/toolkit/query/react'
-import { RootState } from '../app/store';
+import {BaseQueryFn, createApi, FetchArgs, fetchBaseQuery, FetchBaseQueryError} from '@reduxjs/toolkit/query/react'
+import {RootState} from '../app/store';
 import {logout, Role, setToken} from '../features/auth/auth-slice';
 
 interface Profile {
@@ -22,7 +22,7 @@ const baseUrl = import.meta.env.PROD ? '/api/' : 'http://localhost:8080/api/'
 
 const baseQuery = fetchBaseQuery({
     baseUrl,
-    prepareHeaders(headers, { getState }) {
+    prepareHeaders(headers, {getState}) {
         const token = (getState() as RootState).auth.accessToken;
         if (token) {
             headers.set('authorization', `Bearer ${token}`);
@@ -39,9 +39,12 @@ const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQue
 
     if (result.error && result.error.status === 401) {
         if (state.auth.refreshToken) {
-            const refreshResult = await baseQuery({ url: 'auth/refresh', headers: { refresh: state.auth.refreshToken } }, api, extraOptions)
+            const refreshResult = await baseQuery({
+                url: 'auth/refresh',
+                headers: {refresh: state.auth.refreshToken}
+            }, api, extraOptions)
             if (refreshResult.data) {
-                const { token } = refreshResult.data as { token: string }
+                const {token} = refreshResult.data as { token: string }
                 api.dispatch(setToken(token))
 
                 return baseQuery(args, api, extraOptions)
@@ -61,18 +64,48 @@ export const apiSlice = createApi({
     baseQuery: baseQueryWithReauth,
     endpoints(builder) {
         return {
-            login: builder.mutation<{ token: string, "refresh-token": string, role: Role }, { username: string, password: string }>({ query: (credentials) => ({ url: 'auth/login', method: 'POST', body: credentials }) }),
-            event: builder.query<Event, string>({ query: (id) => `events/${id}` }),
-            createEvent: builder.mutation({ query: (event) => ({ url: 'events', method: 'POST', body: event }) }),
-            fetchEvents: builder.query<{events: [Event], page: number, count: number }, number>({ query: (page) => `events?page=${page}` }),
-            fetchUsers: builder.query<{users: [User], page: number, count: number }, number>({ query: (page) => `users?page=${page}` }),
-            registerUser: builder.mutation({ query: (credentials) => ({ url: 'users/register', method: 'POST', body: credentials }) }),
-            profile: builder.query<Profile, void>({ query: () => 'users/profile' }),
-            deleteUser: builder.mutation<void, void>({query: () => ({url: 'users/profile', method: 'DELETE' })}),
-            changePassword: builder.mutation<void, { currentPassword: string, newPassword: string}>({query: (body) => ({ url: 'users/change-password', method: 'POST', body }) })
+            login: builder.mutation<{ token: string, "refresh-token": string, role: Role }, { username: string, password: string }>({
+                query: (credentials) => ({
+                    url: 'auth/login',
+                    method: 'POST',
+                    body: credentials
+                })
+            }),
+            event: builder.query<Event, string>({query: (id) => `events/${id}`}),
+            createEvent: builder.mutation({query: (event) => ({url: 'events', method: 'POST', body: event})}),
+            fetchEvents: builder.query<{ events: [Event], page: number, count: number }, number>({query: (page) => `events?page=${page}`}),
+            createUserByAdmin: builder.mutation({query: (user) => ({url: 'users/create', method: 'POST', body: user})}),
+            fetchUsers: builder.query<{ users: [User], page: number, count: number }, number>({query: (page) => `users?page=${page}`}),
+            registerUser: builder.mutation({
+                query: (credentials) => ({
+                    url: 'users/register',
+                    method: 'POST',
+                    body: credentials
+                })
+            }),
+            profile: builder.query<Profile, void>({query: () => 'users/profile'}),
+            deleteUser: builder.mutation<void, void>({query: () => ({url: 'users/profile', method: 'DELETE'})}),
+            changePassword: builder.mutation<void, { currentPassword: string, newPassword: string }>({
+                query: (body) => ({
+                    url: 'users/change-password',
+                    method: 'POST',
+                    body
+                })
+            })
         }
     }
 });
 
 
-export const { useLoginMutation, useFetchEventsQuery, useFetchUsersQuery, useRegisterUserMutation, useProfileQuery, useEventQuery, useCreateEventMutation, useDeleteUserMutation, useChangePasswordMutation } = apiSlice;
+export const {
+    useLoginMutation,
+    useFetchEventsQuery,
+    useFetchUsersQuery,
+    useRegisterUserMutation,
+    useProfileQuery,
+    useEventQuery,
+    useCreateEventMutation,
+    useCreateUserByAdminMutation,
+    useDeleteUserMutation,
+    useChangePasswordMutation
+} = apiSlice;
