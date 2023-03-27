ddc3577e02c18d6a29cdd1706d4ee7a8d65533b9
Simon Engstler <102315699+SirSiegmeyer@users.noreply.github.com>
2023-03-27 22:27:13 +0200

    created basic event model (#58)

---
+++ scraper/models/event.py
@@ -0,0 +1,12 @@
+class Event():
+    def __init__(self, name, date=None, location=None, price_info=None, organizer=None, link=None):
+        self.name = name
+        self.date = date
+        self.location = location
+        self.price_info = price_info
+        self.organizer = organizer
+        self.link = link
+
+    def __str__(self):
+        return f"Event: {self.name}\nDate: {self.date}\nLocation: {self.location}\nPrice Info: {self.price_info}\n" \
+               f"Organizer: {self.organizer}\nLink: {self.link}"
