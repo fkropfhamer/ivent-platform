7db0613e8e3b2703c02a3bd56d29fc3d0743b4e9
fkropfhamer <fabian.kropfhamer@gmail.com>
2023-04-01 16:16:27 +0200

    fix code style + update .gitignore

--- web/src/routes/CreateEvent.tsx
+++ web/src/routes/CreateEvent.tsx
@@ -1,39 +1,38 @@
-import {useState} from 'react'
-import {useNavigate} from 'react-router-dom'
-import {useCreateEventMutation} from '../services/api'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useCreateEventMutation } from '../services/api'
 
 export const CreateEvent = (): JSX.Element => {
-    const [formState, setFormState] = useState({
-        name: "",
-        date: "",
-        location: "",
-        priceInfo: "",
-        organizer: "",
-        link: ""
+  const [formState, setFormState] = useState({
+    name: '',
+    date: '',
+    location: '',
+    priceInfo: '',
+    organizer: '',
+    link: ''
+  })
+  const [create, _unused] = useCreateEventMutation()
+  const navigate = useNavigate()
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault()
+
+    await create({
+      name: formState.name,
+      date: formState.date,
+      location: formState.location,
+      price_info: formState.priceInfo,
+      organizer: formState.organizer,
+      link: formState.link
     })
-    const [create, _unused] = useCreateEventMutation()
-    const navigate = useNavigate()
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-        e.preventDefault()
 
-        await create({
-            name: formState.name,
-            date: formState.date,
-            location: formState.location,
-            price_info: formState.priceInfo,
-            organizer: formState.organizer,
-            link: formState.link
-        })
+    navigate('/events')
+  }
 
-        navigate("/events")
-
-    }
-
-    return (
+  return (
         <div className="w-96 bg-gray-100 rounded-lg mx-auto my-20 p-8 border-2 border-gray-200">
             <h1 className="text-3xl text-center font-bold mb-6">Create Event</h1>
             <form onSubmit={(e) => {
-                void handleSubmit(e)
+              void handleSubmit(e)
             }} className="flex flex-col">
                 <label className="block text-lg mb-4">
                     Name *
@@ -43,7 +42,7 @@ export const CreateEvent = (): JSX.Element => {
                     className="w-full px-4 py-2 rounded-lg text-lg mb-6 border-2 border-gray-300"
                     value={formState.name}
                     onChange={(e) => {
-                        setFormState((prev) => ({...prev, name: e.target.value}))
+                      setFormState((prev) => ({ ...prev, name: e.target.value }))
                     }}
                     required
                 />
@@ -55,7 +54,7 @@ export const CreateEvent = (): JSX.Element => {
                     className="w-full px-4 py-2 rounded-lg text-lg mb-6 border-2 border-gray-300"
                     value={formState.date}
                     onChange={(e) => {
-                        setFormState((prev) => ({...prev, date: e.target.value}))
+                      setFormState((prev) => ({ ...prev, date: e.target.value }))
                     }}
                     required
                 />
@@ -67,7 +66,7 @@ export const CreateEvent = (): JSX.Element => {
                     className="w-full px-4 py-2 rounded-lg text-lg mb-6 border-2 border-gray-300"
                     value={formState.location}
                     onChange={(e) => {
-                        setFormState((prev) => ({...prev, location: e.target.value}))
+                      setFormState((prev) => ({ ...prev, location: e.target.value }))
                     }}
                 />
                 <label className="block text-lg mb-4">
@@ -78,7 +77,7 @@ export const CreateEvent = (): JSX.Element => {
                     className="w-full px-4 py-2 rounded-lg text-lg mb-6 border-2 border-gray-300"
                     value={formState.priceInfo}
                     onChange={(e) => {
-                        setFormState((prev) => ({...prev, priceInfo: e.target.value}))
+                      setFormState((prev) => ({ ...prev, priceInfo: e.target.value }))
                     }}
                 />
                 <label className="block text-lg mb-4">
@@ -89,7 +88,7 @@ export const CreateEvent = (): JSX.Element => {
                     className="w-full px-4 py-2 rounded-lg text-lg mb-6 border-2 border-gray-300"
                     value={formState.organizer}
                     onChange={(e) => {
-                        setFormState((prev) => ({...prev, organizer: e.target.value}))
+                      setFormState((prev) => ({ ...prev, organizer: e.target.value }))
                     }}
                 />
                 <label className="block text-lg mb-4">
@@ -100,7 +99,7 @@ export const CreateEvent = (): JSX.Element => {
                     className="w-full px-4 py-2 rounded-lg text-lg mb-6 border-2 border-gray-300"
                     value={formState.link}
                     onChange={(e) => {
-                        setFormState((prev) => ({...prev, link: e.target.value}))
+                      setFormState((prev) => ({ ...prev, link: e.target.value }))
                     }}
                 />
                 <button
@@ -111,5 +110,5 @@ export const CreateEvent = (): JSX.Element => {
                 </button>
             </form>
         </div>
-    )
+  )
 }
