4943840dba3ae5ad89abd69cf5808fcbb20bdfc1
Simon Engstler <102315699+SirSiegmeyer@users.noreply.github.com>
2023-04-21 19:39:15 +0200

    improved visuals of index (#93)
    * improved visuals of index
    
    * fixed linter
    
    * update to fix linter
    
    * fixed Linter again

--- web/src/routes/Index.tsx
+++ web/src/routes/Index.tsx
@@ -8,19 +8,34 @@ function Index (): JSX.Element {
   const { data = { count: 0 }, isFetching } = useFetchEventsQuery({ page: 0, marked: undefined })
 
   return (
-        <div className="w-96 bg-white rounded-lg mx-auto my-20 p-8 border-2 border-gray-200">
-            <h1 className="text-3xl text-center font-bold underline mb-6">Welcome to the IVENT-Platform</h1>
+        <div className="w-96 bg-gray-100 rounded-lg mx-auto my-20 p-8 border-2 border-gray-200">
+            <h1 className="text-3xl text-center font-bold mb-6">Welcome to the IVENT-Platform</h1>
             <div className="card">
                 <p className="text-lg text-center">
-                    {isFetching ? 'Loading' : `Total number of events: ${data.count}`}
+                    {isFetching
+                      ? 'Loading'
+                      : (
+                            <>
+                                Home of <span className="text-green-500 font-bold">{data.count}</span> events and
+                                counting
+                            </>
+                        )}
                 </p>
                 <div className="flex justify-center space-x-4 mt-8">
-                    <Link to="/register" className="text-green-500 hover:underline">Register</Link>
-                    <Link to="/login" className="text-green-500 hover:underline">Login</Link>
+                    <Link to="/register"
+                          className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-4 transition-colors duration-300">Register</Link>
+                    <Link to="/login"
+                          className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-4 transition-colors duration-300">Login</Link>
                     {isLoggedIn
-                      ? <Link to="/events" className="text-green-500 hover:underline">Events</Link>
-                      : <></>
-                    }
+                      ? (
+                            <Link to="/events"
+                                  className="bg-green-500 hover:bg-green-600 text-white rounded-lg py-2 px-4 transition-colors duration-300">
+                                Events
+                            </Link>
+                        )
+                      : (
+                            <></>
+                        )}
                 </div>
             </div>
         </div>
